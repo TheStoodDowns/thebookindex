ff769f11f65785ff6b35921cd4c1d83ac0ae701c
Martin <martin@burcombe.net>
2020-07-12 22:48:31 +1000

    Fixing Angular e2e tests

---
+++ TheBookIndex.Web/e2e/protractor-ci.conf.js
@@ -0,0 +1,12 @@
+// angular.io/guide/testing
+
+const config = require('./protractor.conf').config;
+
+config.capabilities = {
+  browserName: 'chrome',
+  chromeOptions: {
+    args: ['--headless', '--no-sandbox']
+  }
+};
+
+exports.config = config;
